c0fd53eefc7689eb4e5ccea60e81e0056be004ae
ZELORD <zelord.kwoun@gmail.com>
2020-09-22 18:41:02 +0900

    .

--- REACT&Movie/movie-app/src/Movie.js
+++ REACT&Movie/movie-app/src/Movie.js
@@ -1,19 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie(id, year, title, summary, poster){
+function Movie({ id, year, title, summary, poster}){
     return <h4>{title}</h4>
 }
 
 Movie.propTypes = {
     id : PropTypes.number.isRequired,
-    year : PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
     poster : PropTypes.string.isRequired,
-    // rating : PropTypes.number.isRequired
+
 }
 
 export default Movie;
+
 // state가 필요하면 class component
 // state가 필요없으면 function component
--- REACT&Movie/movie-app/src/index.js
+++ REACT&Movie/movie-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Potato from './Potato'
+// import Potato from './Potato'
 
 // ReactDOM.render(<App />,document.getElementById('root'));
 ReactDOM.render(<App />,document.getElementById('root'));
