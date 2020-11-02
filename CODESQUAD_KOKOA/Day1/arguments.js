cb17eb8a7dbaa1a47d948fe22bd46392b6b3ecd8
ZELORD <zelord.kwoun@gmail.com>
2020-11-02 16:33:22 +0900

    .

---
+++ CODESQUAD_KOKOA/Day1/arguments.js
@@ -0,0 +1,11 @@
+function getArguments(a){
+    console.log("인자의 갯수는 : ",arguments.length)
+    console.log(arguments[0]);
+    console.log(arguments[1]);
+    console.log(arguments[2]);
+}
+
+
+getArguments(1)
+getArguments(1,2)
+getArguments(1,2,3)
