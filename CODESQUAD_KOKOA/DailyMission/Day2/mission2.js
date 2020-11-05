c8589c3f2d80181339d7cb836347c94caf15dffa
ZELORD <zelord.kwoun@gmail.com>
2020-11-05 10:56:46 +0900

    Update mission2.js

--- CODESQUAD_KOKOA/DailyMission/Day2/mission2.js
+++ CODESQUAD_KOKOA/DailyMission/Day2/mission2.js
@@ -1,3 +1,4 @@
+// 1. factorial_calculator
 function getArrayMultiple(num){
     let temp = 1;
     let arr = [...Array(num)].map((v,i) => i + 1)
@@ -16,8 +17,14 @@ function calculate(n){
     console.log(factorial(n))
 }
 
-// calculate(4)
+calculate(4)
+// ************************************************** //
 
+
+// 2. 배열 거르기 (forEach, filter, map)
+const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
+
+// case1.
 function checkSpecial(str){ 
     let special_pattern = /[`~!@#$%^&*|\\\'\";:\/?]/gi; 
     if(special_pattern.test(str) == false){ return str } 
@@ -28,18 +35,13 @@ function replaceNumber(str){
 }
 
 function filterId(peoples){
-    let result = []
-    peoples.forEach(function (n) {
-        if (checkSpecial(n) !== undefined){
-            result.push(replaceNumber(n))
-        }
-    })
+    result = peoples.filter(checkSpecial).map(replaceNumber)
     console.log(result)
 }
 
-const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
 filterId(peoples)
 
+// case2.
 function filterId2(peoples){
     result = []
     let special_pattern = /[`~!@#$%^&*|\\\'\";:\/?]/gi; 
@@ -51,3 +53,194 @@ function filterId2(peoples){
     console.log(result)
 }
 filterId2(peoples)
+
+// ************************************************** //
+
+
+// 3. 평균 구하기
+const grades = [[88,76,77], [33,44,44], [90,100,94], [30,44,98]];
+
+function getAverage(arr){
+    return parseInt(arr.reduce((a,b) => (a+b)) / arr.length);
+}
+
+function getBestArr(arr){
+    let max = arr.reduce(function(a,b) {
+        return Math.max(a,b);
+    })
+    return max;
+}
+
+function getAverageObj(obj){
+    let sum = 0
+    for(key in obj){
+        sum += obj[key]
+    }
+    return parseInt(sum / obj.length)
+}
+
+
+let averages = grades.map(function(v) {
+    return getAverage(v)
+});
+console.log(averages)
+
+let bestScores = grades.map(function(v) {
+    return getBestArr(v)
+})
+
+let bestAverage = getAverageObj(bestScores)
+
+console.log(bestAverage)
+// ************************************************** //
+
+
+// 4. 배열 만들기
+const data = {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": { 
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+}
+
+result = []
+for (key in data){
+    const sub = data[key]
+    for (a in sub){
+        if (typeof sub[a] === 'number'){
+            result.push(a)
+        }
+    }
+}
+console.log(result)
+
+// ************************************************** //
+
+
+// 5. 배열 결과 출력
+const node = [{
+	"id": 1,
+	"name": "Yong",
+	"phone": "010-0000-0000",
+	"type": "sk",
+	"childnode": [{
+		"id": 11,
+		"name": "echo",
+		"phone": "010-0000-1111",
+		"type": "kt",
+		"childnode": [{
+				"id": 115,
+				"name": "hary",
+				"phone": "211-1111-0000",
+				"type": "sk",
+				"childnode": [{
+					"id": 1159,
+					"name": "pobi",
+					"phone": "010-444-000",
+					"type": "kt",
+					"childnode": [{
+							"id": 11592,
+							"name": "cherry",
+							"phone": "111-222-0000",
+							"type": "lg",
+							"childnode": []
+						},
+						{
+							"id": 11595,
+							"name": "solvin",
+							"phone": "010-000-3333",
+							"type": "sk",
+							"childnode": []
+						}
+					]
+				}]
+			},
+			{
+				"id": 116,
+				"name": "kim",
+				"phone": "444-111-0200",
+				"type": "kt",
+				"childnode": [{
+					"id": 1168,
+					"name": "hani",
+					"phone": "010-222-0000",
+					"type": "sk",
+					"childnode": [{
+						"id": 11689,
+						"name": "ho",
+						"phone": "010-000-0000",
+						"type": "kt",
+						"childnode": [{
+								"id": 116890,
+								"name": "wonsuk",
+								"phone": "010-000-0000",
+								"type": "kt",
+								"childnode": []
+							},
+							{
+								"id": 1168901,
+								"name": "chulsu",
+								"phone": "010-0000-0000",
+								"type": "sk",
+								"childnode": []
+							}
+						]
+					}]
+				}]
+			},
+			{
+				"id": 117,
+				"name": "hong",
+				"phone": "010-0000-0000",
+				"type": "lg",
+				"childnode": []
+			}
+		]
+	}]
+}]
+
+function getChildNode(v){
+    node.map(function (v){
+        return v.childnode
+    })
+}
+
+// console.log(getChildNode(node))
+
+// ************************************************** //
+
+
+// 6. reduce 만들기
+// const myReduce = (arr, callback, initialValue) => {
+//     //여기에 구현
+// }
+
+// const result = myReduce(arr, (next,prev) => {...}, []);
+
+
+// ************************************************** //
+
+
+// 7. 추가미션
+
+// ************************************************** //
