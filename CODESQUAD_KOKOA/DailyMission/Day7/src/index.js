63e6730f68dec426b8efdd849a6f3b851b061de5
ZELORD <zelord.kwoun@gmail.com>
2020-11-26 12:06:26 +0900

    .

--- CODESQUAD_KOKOA/DailyMission/Day7/src/index.js
+++ CODESQUAD_KOKOA/DailyMission/Day7/src/index.js
@@ -1,12 +1,9 @@
-// <⚠️ DONT DELETE THIS ⚠️>
-// import "./styles.css";
-// const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 const FRUITS = ['grape','apple','orange','banana','kiwi','strawberry']
-// const FRUITS = ['김은실','사랑해','김은실','사랑해','김은실','사랑해']
 const BUTTON_COLORS = ["btn-two yellow rounded","btn-two green rounded","btn-two blue rounded","btn-two red rounded","btn-two purple rounded","btn-two cyan rounded"]
 
 const dropbtn = document.querySelector("#mainBtn");
 const dropdown = document.querySelector(".dropdown");
+const dropdownCount = document.querySelector(".dropdownCount");
 let btn0;
 let btn1;
 let btn2;
@@ -14,12 +11,25 @@ let btn3;
 let btn4;
 let btn5;
 
-console.log("dropdown", dropdown)
-console.log("dropbtn", dropbtn)
-// const h2 = document.querySelector("h2");
 let printed;
+let counting;
+function printCounts() {
+  FRUITS.map( function (v, i) {
+    let li = document.createElement("li");
+    let span1 = document.createElement("span");
+    let span2 = document.createElement("span");
 
-function print(){
+    span1.innerText = `${FRUITS[i]} : `
+    span2.innerText = 0
+    span2.id = `value${i}`
+    li.appendChild(span1)
+    li.appendChild(span2)
+    dropdownCount.append(li)
+
+  })
+}
+
+function printButtons(){
   printed = true
   FRUITS.map( function (v, i){
     let a = document.createElement('a');
@@ -28,6 +38,7 @@ function print(){
     a.id = 'btn'+i
     dropdown.appendChild(a)
   })
+  printCounts()
   createQuerySelector()
 }
 
@@ -39,33 +50,44 @@ function createQuerySelector(){
   btn4 = document.querySelector("#btn4")
   btn5 = document.querySelector("#btn5")
 
-  btn0.addEventListener("mouseenter", handleEnterCount);
-  btn1.addEventListener("mouseenter", handleEnterCount);
-  btn2.addEventListener("mouseenter", handleEnterCount);
-  btn3.addEventListener("mouseenter", handleEnterCount);
-  btn4.addEventListener("mouseenter", handleEnterCount);
-  btn5.addEventListener("mouseenter", handleEnterCount);
+  btn0.addEventListener("mouseenter", function() {handleEnterCount(1)});
+  btn1.addEventListener("mouseenter", function() {handleEnterCount(2)});
+  btn2.addEventListener("mouseenter", function() {handleEnterCount(3)});
+  btn3.addEventListener("mouseenter", function() {handleEnterCount(4)});
+  btn4.addEventListener("mouseenter", function() {handleEnterCount(5)});
+  btn5.addEventListener("mouseenter", function() {handleEnterCount(6)});
 
+  btn0.addEventListener("mouseleave", handleLeaveCount);
+  btn1.addEventListener("mouseleave", handleLeaveCount);
+  btn2.addEventListener("mouseleave", handleLeaveCount);
+  btn3.addEventListener("mouseleave", handleLeaveCount);
+  btn4.addEventListener("mouseleave", handleLeaveCount);
+  btn5.addEventListener("mouseleave", handleLeaveCount);
 }
 
 function handleEnter() {
   console.log('mouse enter')
   if (printed !== true){
-    printed = setTimeout(print, 1000);
+    printed = setTimeout(printButtons, 1000);
   }
 }
 
-
-function handleLeave(countOneSec) {
-  console.log('mouse leave')
+function handleLeave() {
   clearTimeout(printed)
-
 }
 
-function handleEnterCount() {
-  console.log("AAA")
+function handleEnterCount(value) {
+  counting = setTimeout(countPlus.bind(null, value),1000)
 }
 
+function handleLeaveCount() {
+  clearTimeout(counting)
+  
+}
+
+function countPlus(value){
+  dropdownCount.childNodes[value].childNodes[1].innerText = parseInt(dropdownCount.childNodes[value].childNodes[1].innerText) + 1
+}
 
 dropbtn.addEventListener("mouseenter", handleEnter);
 dropbtn.addEventListener("mouseleave", handleLeave);
