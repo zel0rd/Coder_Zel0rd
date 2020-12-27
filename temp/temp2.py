f08c94e0f15b92a4ca7a6566d31f9dc48f6d8f39
younggil <zelord.kwoun@gmail.com>
2020-12-28 00:57:06 +0900

    .

---
+++ temp/temp2.py
@@ -0,0 +1,10 @@
+list = [1,11,111,1111]
+# for i in list:
+#     print(f'9 * {i} = {9*i}')
+
+# for i in list:
+#     print(f'9 * {i:4} = {9*i}')
+
+for i in range(2,10):
+    for j in range(1,10):
+        print(f'{i} * {j} = {i*j}')
