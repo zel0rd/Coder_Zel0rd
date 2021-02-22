f287162fe7b34dd613d391fdc5fa9b66e0623514
younggil <zelord.kwoun@gmail.com>
2021-02-22 19:11:21 +0900

    .

--- CODESQUAD_MASTER/shoppinghow/app.js
+++ CODESQUAD_MASTER/shoppinghow/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+// var sassMiddleware = require('node-sass-middleware')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -19,6 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(sassMiddleware({
+// }))
+
+// app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
