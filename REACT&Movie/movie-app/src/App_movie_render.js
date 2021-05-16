2f20a75f5f16955a30da42394fdb3f06fab1caf1
younggil <zelord.kwoun@gmail.com>
2021-05-16 10:42:10 +0900

    Revert "[ADD] create new project react_movie"
    This reverts commit de6db38523990127a27936076c2f28a88d9c9a83.

---
+++ REACT&Movie/movie-app/src/App_movie_render.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from "axios";
+import Movie from "./Movie"
+
+class App extends React.Component{
+  state = {
+    isLoading : true,
+    movies : []
+  }
+  getMovies = async() => {
+    const {data: { data: { movies }}} = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+    console.log(movies)
+    this.setState({ movies, isLoading: false })
+  }
+  componentDidMount() {
+    // setTimeout(() => {
+    //   this.setState({ isLoading: false })
+    // }, 2000)
+    
+    this.getMovies()
+  }
+  render() {
+    const { isLoading, movies } = this.state
+    // return <div>{isLoading ? "Loading..." : "We are ready" } </div>
+    return <div>{isLoading 
+      ? "Loading..." 
+      : movies.map(movie => (
+          <Movie
+            key = {movie.id}
+            id = {movie.id}
+            year = {movie.year}
+            title = {movie.title}
+            summary = {movie.summary}
+            poster = {movie.medium_cover_image}
+          />
+        )
+      )}
+    </div>
+  }
+}
+
+export default App;
+
+// fetch
+// axios
+// api-url : https://yts-proxy.now.sh/list_movies.json
