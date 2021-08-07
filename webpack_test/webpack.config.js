c8f0ace6ea5634a2bb866a0f04de62f02c110e86
younggil <zelord.kwoun@gmail.com>
2021-08-07 12:34:16 +0900

    [ADD} webpack_test

---
+++ webpack_test/webpack.config.js
@@ -0,0 +1,43 @@
+// export
+const path = require('path')
+const HtmlPlguin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: './js/main.js',
+    
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        clean: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins : [
+        new HtmlPlguin({
+            template: './index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'static' }
+            ]
+        })
+    ],
+
+    devServer: {
+        host: 'localhost'
+    }
+}
