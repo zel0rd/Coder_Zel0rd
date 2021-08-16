692c7854324e6fbe055bd56ebcbd5123dea5fe2e
younggil <zelord.kwoun@gmail.com>
2021-08-16 19:04:36 +0900

    [ADD] fastcampus

---
+++ VUE_STUDY/fastcampus/vue/vue3-webpack-template/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: "hello vue3!!"
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
